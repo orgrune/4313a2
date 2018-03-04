08880e3a8beed5d13ce410d12012befbcdcaab54
minh <m1ph@protonmail.com>
2018-03-04 11:34:07 -0500

    Switch ECT method to TaskModel.daysBetween instead of DateUtil.isAfter

--- AutoTest/eecs4313a2w/EquivalentClassTest.java
+++ AutoTest/eecs4313a2w/EquivalentClassTest.java
@@ -1,33 +1,40 @@
-package eecs4313a2w;
+package eecs4313a2b;
 
 import static org.junit.Assert.*;
 
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import net.sf.borg.common.DateUtil;
+import net.sf.borg.model.TaskModel;
+import net.sf.borg.model.db.DBHelper;
+import net.sf.borg.model.db.jdbc.JdbcDBHelper;
 
 public class EquivalentClassTest {
-	Date d1 = new Date();
-	Date d2 = new Date();
 	
-	@Before
-	public void setUp() throws Exception {
-		d2.setMonth(d1.getMonth() + 1);;
+	@BeforeClass
+	public static void setUp() throws Exception {
+		// open the borg dbs - in memory
+		DBHelper.setFactory(new JdbcDBHelper());
+		DBHelper.setController(new JdbcDBHelper());
+		DBHelper.getController().connect("jdbc:hsqldb:mem:whatever");
 	}
 
 	@Test
-	public void testIsAfter() {
+	public void testDaysBetween() {
 		// R1 = {d1 equals d2}
 		// R2 = {d1 is before d2}
 		// R3 = {d1 is after d2}
+		Date d1 = new Date(2018-1900, 1, 1);
+		Date d2 = new Date(2018-1900, 1, 2);
 		
-		assertFalse(DateUtil.isAfter(d1, d1));
+		assertEquals(1, TaskModel.daysBetween(d1, d2));
 		
-		assertFalse(DateUtil.isAfter(d2, d1));
+		assertEquals(0, TaskModel.daysBetween(d1, d1));
 		
-		assertTrue(DateUtil.isAfter(d1, d2));
+		assertEquals(-1, TaskModel.daysBetween(d2, d1));
 	}
 }
